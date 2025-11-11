55cad714fa697cb258226ec1cb042177656a917b
yethantzaw-alt <yethantzaw@gmail.com>
2025-11-11 15:50:59 +0630

    data type

--- data_type.py
+++ data_type.py
@@ -1,6 +1,9 @@
 age = 30
 amount = 35.75
 name = "ye thant zaw"
+a = True
 print(type(age),"age data type")
 print(type(amount),"amount data type")
 print(type(name),"name data type")
+print (type(a), "name data type")
+
